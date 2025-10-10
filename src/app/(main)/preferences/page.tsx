0678096181dbfc1dfe8743a07f5878403f5f2b61
João Saldanha <jlucassaldanha@gmail.com>
2025-10-10 19:11:05 -0300

    Correção para carregamento do banco de dados

--- src/app/(main)/preferences/page.tsx
+++ src/app/(main)/preferences/page.tsx
@@ -4,8 +4,12 @@ import { getUserPreferences } from "@/core/preferences/preferences.services";
 import { minutesToTimeString } from "@/lib/timeFormater";
 import { redirect } from "next/navigation";
 
-
 export const dynamic = 'force-dynamic'
+
+const dayNumberToKeyMap: { [key: number]: string } = {
+  0: 'sunday', 1: 'monday', 2: 'tuesday', 3: 'wednesday',
+  4: 'thursday', 5: 'friday', 6: 'saturday',
+}
 
 export default async function Preferences() {
 	const session = await auth();
@@ -18,28 +22,44 @@ export default async function Preferences() {
 	
 	  const userPreferences = await getUserPreferences(userId)
 
-	  const initialSchedules = userPreferences?.dailySchedules.map(schedule => {
-		return {
-			id: Date.now().toString(),
-			entryTime: minutesToTimeString(schedule.entryTime),
-			exitTime: minutesToTimeString(schedule.exitTime),
-			lunchStartTime: minutesToTimeString(schedule.lunchStartTime),
-			lunchEndTime: minutesToTimeString(schedule.lunchEndTime),
-			days: {
-				sunday: schedule.dayOfWeek === 0,
-				monday: schedule.dayOfWeek === 1,
-				tuesday: schedule.dayOfWeek === 2,
-				wednesday: schedule.dayOfWeek === 3,
-				thursday: schedule.dayOfWeek === 4,
-				friday: schedule.dayOfWeek === 5,
-				saturday: schedule.dayOfWeek === 6,
-			}
+	  const dailySchedulesFromDb = userPreferences?.dailySchedules || []
+
+  	  const scheduleRulesMap = new Map<string, any>();
+	  
+
+	  for (const schedule of dailySchedulesFromDb) {
+		const timeKey = `${schedule.entryTime}-${schedule.exitTime}-${schedule.lunchStartTime}-${schedule.lunchEndTime}`;
+
+		if (!scheduleRulesMap.has(timeKey)) {
+			scheduleRulesMap.set(timeKey, {
+				id: Date.now().toString(),
+				entryTime: minutesToTimeString(schedule.entryTime),
+				exitTime: minutesToTimeString(schedule.exitTime),
+				lunchStartTime: minutesToTimeString(schedule.lunchStartTime),
+				lunchEndTime: minutesToTimeString(schedule.lunchEndTime),
+				days: { 
+					sunday: false, 
+					monday: false, 
+					tuesday: false, 
+					wednesday: false, 
+					thursday: false, 
+					friday: false, 
+					saturday: false 
+				},
+			});
 		}
-	  }) || []
+
+		const dayKey = dayNumberToKeyMap[schedule.dayOfWeek];
+		if (dayKey) {
+			scheduleRulesMap.get(timeKey).days[dayKey] = true;
+		}
+	}
+
+	const initialSchedulesForForm = Array.from(scheduleRulesMap.values());
 
 	return (
 		<div className="flex flex-col items-center justify-center w-full gap-2">
-			<PreferencesForm initialSchedules={initialSchedules} />			
+			<PreferencesForm initialSchedules={initialSchedulesForForm} />			
 		</div>
 	)
 }
