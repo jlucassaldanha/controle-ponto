da459505a8305332641e867c3c8f5f0b0cffbd6e
João Saldanha <jlucassaldanha@gmail.com>
2025-10-20 21:33:14 -0300

    Função de formatar ponto

--- src/app/(main)/punch/page.tsx
+++ src/app/(main)/punch/page.tsx
@@ -1,4 +1,5 @@
 import { getPunches } from "@/core/punch/punch.services"
+import { formatPunch } from "@/lib/formatPunch"
 import { requireUserSession } from "@/lib/session"
 
 export const dynamic = 'force-dynamic'
@@ -7,22 +8,21 @@ export default async function Punch() {
 	const session = await requireUserSession()
 	const punches = await getPunches(session.id)
 
+
+
 	return (
 		<div className="flex flex-col items-center justify-center w-full gap-2">
-			{punches.map((punch) => (
-				<div key={punch.id} className="flex flex-col">
-					{punch.type}
-					<div>
-						{punch.timestamp.getUTCDate()}/
-						{punch.timestamp.getUTCMonth() + 1}/
-						{punch.timestamp.getUTCFullYear()}
+			{punches.map((punch) => {
+				const formatedPunch = formatPunch(punch)
+
+				return (
+					<div key={punch.id} className="flex flex-col gap-5">
+						{formatedPunch.type} 
+						{formatedPunch.date} 
+						{formatedPunch.time} 
 					</div>
-					<div>
-						{punch.timestamp.getUTCHours()}:
-						{punch.timestamp.getUTCMinutes()}
-					</div>
-				</div>
-			))}
+				)
+			})}
 		</div>
 	)
 }
