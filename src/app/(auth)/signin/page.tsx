afb71d5da41a4d57ca15afa8244fbd59a152df56
João Saldanha <jlucassaldanha@gmail.com>
2025-10-25 01:55:59 -0300

    Pagina inicial e de login

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,17 +1,26 @@
 "use client";
 import { signUpAction, SignUpFormState } from "@/actions/auth.action";
 import SubmitButton from "@/components/ui/SubmitButton";
+import { Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
+import { redirect } from "next/navigation";
 import { useActionState } from "react";
 
 export default function SignUp() {
   const initialState: SignUpFormState = { success: false };
   const [state, formAction] = useActionState(signUpAction, initialState);
 
+  state.success && redirect('/login')
   return (
-    <section className="flex justify-center items-center w-full h-screen">
+    <section className="flex flex-col justify-center items-center w-full h-screen">
+      <Typography 
+        variant="h3" 
+        component="h2" 
+      >
+        Criar conta
+      </Typography>
       <form action={formAction}>
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 m-9">
           <TextField
             variant="outlined"
             label="Nome"
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,43 @@
+import { Box, Typography, Container } from '@mui/material';
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <Box
+      className="bg-[url('/hero.jpg')] bg-cover bg-center flex flex-col items-center justify-center gap-7 w-full "
+    >
+      <Container maxWidth="md">
+        <Box className="text-center text-white p-16 rounded-lg flex flex-col gap-5" sx={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+          <Typography 
+            variant="h2" 
+            component="h1" 
+            className="font-bold mb-4"
+          >
+            Controle de ponto 2000
+          </Typography>
+          <Typography 
+            variant="h5" 
+            component="p" 
+            className="mb-8"
+          >
+            Faça o seu controle de ponto e horas extras muito mais fácil
+          </Typography>
+          <div className="flex gap-5 justify-center">
+            <Link 
+              href='/signin'
+              className="bg-blue-500 p-3 rounded-md"
+            >
+              Começar Agora
+            </Link>
+            <Link 
+              href="/login"
+              className="bg-blue-500 p-3 rounded-md"
+            >
+              Já tenho conta
+            </Link>
+          </div>
+        </Box>
+      </Container>
+    </Box>
   );
-}
+};
