afdbbb75cfd1a51f744754846f54a0db96357fcc
João Saldanha <jlucassaldanha@gmail.com>
2025-10-26 14:21:40 -0300

    aviso tela inicial

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,6 @@
 import { Box, Typography, Container } from '@mui/material';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import Link from "next/link";
 
 export default function Home() {
@@ -7,13 +9,13 @@ export default function Home() {
       className="bg-[url('/hero.jpg')] bg-cover bg-center flex flex-col items-center justify-center gap-7 w-full "
     >
       <Container maxWidth="md">
-        <Box className="text-center text-white p-16 rounded-lg flex flex-col gap-5" sx={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        <Box className="text-center text-white p-16 rounded-lg flex flex-col gap-5" sx={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
           <Typography 
             variant="h2" 
             component="h1" 
             className="font-bold mb-4"
           >
-            Controle de ponto 2000
+            Controle de ponto
           </Typography>
           <Typography 
             variant="h5" 
@@ -34,6 +36,40 @@ export default function Home() {
               className="bg-blue-500 p-3 rounded-md"
             >
               Já tenho conta
+            </Link>
+          </div>
+        </Box>
+      </Container>
+      <Container maxWidth="md">
+        <Box className="text-center text-white p-16 rounded-lg flex flex-col gap-5" sx={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+          <Typography 
+            variant="h4" 
+            component="h2" 
+            className="font-bold mb-4"
+          >
+            Aviso!
+          </Typography>
+          <Typography 
+            variant="h6" 
+            component="p" 
+            className="mb-8"
+          >
+            Este projeto ainda está em desenvolvimento, qualquer dúvida, sugestão, ou relato de bug, entre em contato com o desenvolvedor.
+          </Typography>
+          <div className="flex gap-5 justify-center">
+            <Link 
+              href='/signin'
+              className='flex gap-2 justify-center items-center'
+            >
+              <LinkedInIcon/>
+              Linkedin
+            </Link>
+            <Link 
+              href="/login"
+              className='flex gap-2 justify-center items-center'
+            >
+              <GitHubIcon/>
+              GitHub
             </Link>
           </div>
         </Box>
