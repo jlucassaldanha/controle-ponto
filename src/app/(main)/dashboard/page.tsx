eecd345816d7d17e36f6efa77776903012a47dd4
João Saldanha <jlucassaldanha@gmail.com>
2025-10-26 12:57:14 -0300

    Atualizando dashboard e espelho ponto

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { getUserPreferences } from "@/core/preferences/preferences.services";
 import { requireUserSession } from "@/lib/session";
+import { Card, CardContent, CardHeader, Divider } from "@mui/material";
 
 export const dynamic = "force-dynamic";
 
@@ -11,45 +12,48 @@ export default async function Dashboard() {
 
   return (
     <div className="flex flex-col justify-center items-center w-full">
-      <div className="flex flex-col border-[1px] border-gray-300 rounded-md p-3 gap-2 m-8">
-        <span className="flex justify-center border-b-[1px] border-gray-300 pb-2">Opções</span>
-        {userPreferences ? (
-          <div className="flex flex-col">
-            <Link 
-              className="text-blue-500 p-4 hover:bg-blue-50 rounded-md"
-              href='/preferences'
-            >
-              Editar preferencias
-            </Link>
-            <Link 
-              className="text-blue-500 p-4 hover:bg-blue-50 rounded-md"
-              href='/punch/add'
-            >
-              Adicionar ponto
-            </Link>
-            <Link 
-              className="text-blue-500 p-4 hover:bg-blue-50 rounded-md"
-              href='/punch'
-            >
-              Visualizar pontos
-            </Link>
-          </div>
-          ) : (
-            <div className="flex flex-col items-center gap-2">
-              <p className="text-2xl mx-5">
-                Você ainda não tem suas preferencias configuradas nem pontos registrados.
-              </p>
+      <Card>
+        <CardHeader 
+          title="Opções"
+        />
+        <Divider />
+        <CardContent>
+          {userPreferences ? (
+            <div className="flex flex-col">
               <Link 
                 className="text-blue-500 p-4 hover:bg-blue-50 rounded-md"
                 href='/preferences'
               >
                 Editar preferencias
               </Link>
+              <Link 
+                className="text-blue-500 p-4 hover:bg-blue-50 rounded-md"
+                href='/punch/add'
+              >
+                Adicionar ponto
+              </Link>
+              <Link 
+                className="text-blue-500 p-4 hover:bg-blue-50 rounded-md"
+                href='/punch'
+              >
+                Visualizar pontos
+              </Link>
             </div>
-          )}
-        
-      </div>
-      
+            ) : (
+              <div className="flex flex-col items-center gap-2">
+                <p className="text-2xl mx-5">
+                  Você ainda não tem suas preferencias configuradas nem pontos registrados.
+                </p>
+                <Link 
+                  className="text-blue-500 p-4 hover:bg-blue-50 rounded-md"
+                  href='/preferences'
+                >
+                  Editar preferencias
+                </Link>
+              </div>
+            )}
+        </CardContent>
+      </Card>      
     </div>
   );
 }
--- src/app/(main)/punch/page.tsx
+++ src/app/(main)/punch/page.tsx
@@ -11,6 +11,7 @@ import { type Punch, PunchType } from "@prisma/client";
 import { getUserPreferences } from "@/core/preferences/preferences.services";
 import { overtimeUndertime, getPunchTime, getTotalOvertime } from "@/core/punch/punch.utils";
 import { getDailySchedulesTime } from "@/core/preferences/preferences.utils";
+import { Card, CardContent, CardHeader, Divider, Typography } from "@mui/material";
 
 export const dynamic = 'force-dynamic'
 
@@ -31,74 +32,151 @@ export default async function Punch() {
 	}
 
 	return (
-		<div className="flex items-center justify-center w-full gap-2">
-			<TableContainer component={Paper}  sx={{ maxHeight: 600, maxWidth: 800 }} >
-				<Table stickyHeader sx={{minWidth: 400}} arial-label="tabela simples">
-					<TableHead>
-						<TableRow>
-							<TableCell>Data</TableCell>
-							<TableCell align="center">Entrada</TableCell>
-							<TableCell align="center">Entrada almoço</TableCell>
-							<TableCell align="center">Saída almoço</TableCell>
-							<TableCell align="center">Saída</TableCell>
-							<TableCell align="center">Total</TableCell>
-							<TableCell align="center">Extras</TableCell>
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{punchesPerDay.map((day) => {
-							const daySchedule = DailySchedulesTime.find((schedule) => schedule.dayOfWeek === day.dayOfWeek.day)
+		<div className="flex flex-col items-center justify-center w-full gap-5 m-5">
+			<Typography variant="h4" component="h1" className="mb-6 text-center">
+				Espelho Ponto
+			</Typography>
+			<Card>
+				<CardHeader
+					title="Horas Extras/Faltantes"
+				/>
+				<Divider />
+				<CardContent>
+					<p className={"text-" + color + "-500 flex items-center justify-center p-2"}>
+						{totalOvertimeData.timeStr}	
+					</p>
+				</CardContent>
+			</Card>
+			<div className="hidden md:flex">
+				<TableContainer component={Paper}  sx={{ maxHeight: 600, maxWidth: 800 }} >
+					<Table stickyHeader sx={{minWidth: 400}} arial-label="tabela simples">
+						<TableHead>
+							<TableRow>
+								<TableCell>Data</TableCell>
+								<TableCell align="center">Entrada</TableCell>
+								<TableCell align="center">Entrada almoço</TableCell>
+								<TableCell align="center">Saída almoço</TableCell>
+								<TableCell align="center">Saída</TableCell>
+								<TableCell align="center">Total</TableCell>
+								<TableCell align="center">Extras</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{punchesPerDay.map((day) => {
+								const daySchedule = DailySchedulesTime.find((schedule) => schedule.dayOfWeek === day.dayOfWeek.day)
 
-							const workTime = daySchedule ? daySchedule.workTime : 0
-							const overUnder = overtimeUndertime(workTime, day.workedTime.time)
-							
-							let color = ''
-							if (overUnder.overtime && !overUnder.undertime) {
-								color = "green"
-							} else if (!overUnder.overtime && overUnder.undertime) {
-								color = "red"
-							}
-							
-							return (
-								<TableRow
-									key={day.date}
-									sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-								>
-									<TableCell component="th" scope="row">
-										{day.date} <br/> {day.dayOfWeek.dayString}
-									</TableCell>
-									<TableCell align="center">
-										{getPunchTime(day.punches, PunchType.CLOCK_IN)}
-									</TableCell>
-									<TableCell align="center">
-										{getPunchTime(day.punches, PunchType.START_LUNCH)}
-									</TableCell>
-									<TableCell align="center">
-										{getPunchTime(day.punches, PunchType.END_LUNCH)}
-									</TableCell>
-									<TableCell align="center">
-										{getPunchTime(day.punches, PunchType.CLOCK_OUT)}
-									</TableCell>
-									<TableCell align="center">
-										{day.workedTime.timeString}
-									</TableCell>
-									<TableCell align="center" sx={{color: color}}>
-										{overUnder.timeStr}
-									</TableCell>
-								</TableRow>
-							)
-						})}
-					</TableBody>
-				</Table>
-			</TableContainer>
-			<div className="m-7 border-[1px] border-gray-400 rounded-md p-3">
-				<div className="border-b-[1px] border-gray-400 p-2">
-					Total de horas extras ou faltantes
-				</div>
-				<p className={"text-" + color + "-500 flex items-center justify-center p-2"}>
-					{totalOvertimeData.timeStr}	
-				</p>
+								const workTime = daySchedule ? daySchedule.workTime : 0
+								const overUnder = overtimeUndertime(workTime, day.workedTime.time)
+								
+								let color = ''
+								if (overUnder.overtime && !overUnder.undertime) {
+									color = "green"
+								} else if (!overUnder.overtime && overUnder.undertime) {
+									color = "red"
+								}
+								
+								return (
+									<TableRow
+										key={day.date}
+										sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+									>
+										<TableCell component="th" scope="row">
+											{day.date} <br/> {day.dayOfWeek.dayString}
+										</TableCell>
+										<TableCell align="center">
+											{getPunchTime(day.punches, PunchType.CLOCK_IN)}
+										</TableCell>
+										<TableCell align="center">
+											{getPunchTime(day.punches, PunchType.START_LUNCH)}
+										</TableCell>
+										<TableCell align="center">
+											{getPunchTime(day.punches, PunchType.END_LUNCH)}
+										</TableCell>
+										<TableCell align="center">
+											{getPunchTime(day.punches, PunchType.CLOCK_OUT)}
+										</TableCell>
+										<TableCell align="center">
+											{day.workedTime.timeString}
+										</TableCell>
+										<TableCell align="center" sx={{color: color}}>
+											{overUnder.timeStr}
+										</TableCell>
+									</TableRow>
+								)
+							})}
+						</TableBody>
+					</Table>
+				</TableContainer>
 			</div>
+			<div className="md:hidden">
+				<TableContainer component={Paper}  sx={{ maxHeight: 600, maxWidth: 400 }} >
+					<Table stickyHeader sx={{minWidth: 200}} arial-label="tabela simples">
+						<TableHead>
+							<TableRow>
+								<TableCell>Data</TableCell>
+								<TableCell sx={{display: "flex", justifyContent: "space-between", gap: "10px"}} >
+									<span>Entrada</span>
+									<span>Saída</span>
+								</TableCell>
+								<TableCell align="center">Total</TableCell>
+								<TableCell align="center">Extras</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
+							{punchesPerDay.map((day) => {
+								const daySchedule = DailySchedulesTime.find((schedule) => schedule.dayOfWeek === day.dayOfWeek.day)
+
+								const workTime = daySchedule ? daySchedule.workTime : 0
+								const overUnder = overtimeUndertime(workTime, day.workedTime.time)
+								
+								let color = ''
+								if (overUnder.overtime && !overUnder.undertime) {
+									color = "green"
+								} else if (!overUnder.overtime && overUnder.undertime) {
+									color = "red"
+								}
+								
+								return (
+									<TableRow
+										key={day.date}
+										sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+									>
+										<TableCell component="th" scope="row">
+											{day.dayOfWeek.dayString} <br/> {day.date.slice(0, 5)}
+										</TableCell>
+
+										<TableCell sx={{ display: 'flex', flexDirection: "column", gap: "10px" }}>
+											<div className="flex justify-between">
+												<span>
+													{getPunchTime(day.punches, PunchType.CLOCK_IN)}
+												</span>
+												<span>
+													{getPunchTime(day.punches, PunchType.CLOCK_OUT)}
+												</span>
+											</div>
+											<div className="flex justify-between">
+												<span>
+													{getPunchTime(day.punches, PunchType.START_LUNCH)}
+												</span>
+												<span>
+													{getPunchTime(day.punches, PunchType.END_LUNCH)}
+												</span>
+											</div>
+										</TableCell>
+										
+										<TableCell align="center">
+											{day.workedTime.timeString}
+										</TableCell>
+										<TableCell align="center" sx={{color: color}}>
+											{overUnder.timeStr}
+										</TableCell>
+									</TableRow>
+								)
+							})}
+						</TableBody>
+					</Table>
+				</TableContainer>
+			</div>				
 		</div>
 	)
 }
