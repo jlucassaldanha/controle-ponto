c1f591ee72f810822d299460e2651222d02dcab0
João Lucas Saldanha <jlucassaldanha@gmail.com>
2025-11-05 17:07:35 -0300

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,6 +6,7 @@ import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
 import HomeIcon from '@mui/icons-material/Home';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import UpdateModal from "../modal/UpdateModal";
+import { IconButton } from "@mui/material";
 
 
 export default async function Header() {
@@ -21,7 +22,7 @@ export default async function Header() {
         {user ? (
           <div className="flex items-center gap-3">
             <UpdateModal />
-            <Link href="/dashboard">
+            <Link href='/dashboard' className="hover:bg-blue-50 rounded-[50%] p-2">
               <HomeIcon color="primary"/>
             </Link>
             <form action={logOutAction}>
