ef60152ec38c1cc446ac5413f4e7fca92c3c7f67
João Lucas Saldanha <jlucassaldanha@gmail.com>
2025-11-06 17:54:14 -0300

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,13 @@
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import { CircularProgress } from '@mui/material';
 
 export default function Loading() {
 	return (
 		<div className='w-full h-screen flex flex-col gap-2 justify-center items-center'>
-			<div className='animate-pulse'>
+			{/*<div className='animate-pulse'>
 				<AccessAlarmIcon sx={{fontSize: 60}}/>
-			</div>
+			</div>*/}
+			<CircularProgress />
 			<span className='text-2xl'>Carregando</span>		
 		</div>
 	)
