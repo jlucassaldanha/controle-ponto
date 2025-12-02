77ebd6bc3b51e71ce77758d3649cdeae6458452f
João Lucas Saldanha <jlucassaldanha@gmail.com>
2025-12-02 17:38:24 -0300

    Adicionando mudança de tema

--- src/components/ui/Profile.tsx
+++ src/components/ui/Profile.tsx
@@ -10,8 +10,12 @@ import { logOutAction } from "@/actions/auth.action";
 import { useState } from "react"
 import { redirect } from "next/navigation";
 import { ProfileProps } from "./types";
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import { useThemeMode } from "@/providers/ThemeProvider";
 
 export default function Profile({ username }: ProfileProps) {
+	const { toggleTheme, mode } = useThemeMode();
+
 	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
 	const open = Boolean(anchorEl)
 	
@@ -74,6 +78,14 @@ export default function Profile({ username }: ProfileProps) {
 					</ListItemIcon>
 					<ListItemText>
 						Configurações
+					</ListItemText>
+				</MenuItem>
+				<MenuItem>
+					<ListItemIcon>
+						<DarkModeIcon fontSize="small"/>
+					</ListItemIcon>
+					<ListItemText onClick={toggleTheme}>
+						Mudar tema
 					</ListItemText>
 				</MenuItem>
 				<Divider />
