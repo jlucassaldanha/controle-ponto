8e69be54271d87c13df04af35d5ff63112487108
João Lucas Saldanha <jlucassaldanha@gmail.com>
2025-12-10 16:55:50 -0300

    action para abono

--- src/actions/justification.actions.ts
+++ src/actions/justification.actions.ts
@@ -1,5 +1,31 @@
 "use server"
 
-export async function fullDayJustificationAction() {
+import { auth } from "@/app/api/auth/[...nextauth]/route"
+import { createJustification, deleteJustification, findJustification } from "@/core/justification/justification.services"
+import { revalidatePath } from "next/cache"
+
+export async function fullDayJustificationAction(date: Date, time: number) {
+	const session = await auth()
 	
+	if (!session?.user?.id) {
+		return { success: false, message: "Acesso negado." }
+	}
+
+	try {
+		const existingJustification = await findJustification(session.user.id, date)
+		let message: string
+		if (existingJustification) {
+			await deleteJustification(existingJustification.id)
+			message = "Abono apagado"
+		} else {
+			await createJustification(session.user.id, date, time)
+			message = "Abono criado"
+		}
+		
+		revalidatePath("/punch/history")
+		return { success: true, message }
+	} catch (error) {
+		console.log(error)
+		return { success: false, message: "Erro ao tentar salvar abono"}
+	}
 }
