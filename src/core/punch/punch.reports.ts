3316cbb34f39c02978d0adedc18e8941be91cc29
João Lucas Saldanha <jlucassaldanha@gmail.com>
2026-02-03 14:55:12 -0300

    Correção erros de timezone

--- src/core/punch/punch.reports.ts
+++ src/core/punch/punch.reports.ts
@@ -1,65 +1,78 @@
 import { getPunches } from "./punch.services";
-import { minutesToTimeString } from "@/lib/dateUtils"
+import { minutesToTimeString } from "@/lib/dateUtils";
 import { formatDate } from "@/lib/dateUtils";
-import { dailySchedulesTimeType, GroupedPunchesType, PunchesPerDayType } from "./punch.types";
+import {
+  dailySchedulesTimeType,
+  GroupedPunchesType,
+  PunchesPerDayType,
+} from "./punch.types";
 import { getPunchTimestampMinutes, isUnderOver } from "./punch.utils";
 import { PunchType } from "@prisma/client";
 import { JustificationByDayType } from "../justification/justification.types";
 
 export async function groupPunchesByDay(userId: string) {
-	const allPunches = await getPunches(userId)
-	
-	if (allPunches.length === 0) {
-        return [] 
-    }
+  const allPunches = await getPunches(userId);
 
-	const groupedPunches = allPunches.reduce((accumulator, punch) => {
-		const date = formatDate(punch.timestamp)
+  if (allPunches.length === 0) {
+    return [];
+  }
 
-		if (!accumulator[date]) {
-			accumulator[date] = {
-				dayOfWeek: punch.timestamp.getDay(),
-				timestamp: new Date(punch.timestamp.getFullYear(), punch.timestamp.getMonth(), punch.timestamp.getDate()),
-				date,
-				punches: []
-			}
-		}
+  const groupedPunches = allPunches.reduce(
+    (accumulator, punch) => {
+      // punch.timestamp vem do banco em UTC
+      // getDate(), getDay(), getMonth() já retornam no timezone local do navegador
+      const date = formatDate(punch.timestamp);
 
-		accumulator[date].punches.push(punch)
-		return accumulator
-	}, {} as Record<string, GroupedPunchesType>) 
-	
-	const punchesArray = Object.values(groupedPunches).reverse()
+      if (!accumulator[date]) {
+        accumulator[date] = {
+          dayOfWeek: punch.timestamp.getDay(),
+          timestamp: new Date(
+            punch.timestamp.getFullYear(),
+            punch.timestamp.getMonth(),
+            punch.timestamp.getDate(),
+          ),
+          date,
+          punches: [],
+        };
+      }
 
-	const result = punchesArray.map((punchesObj) => {
-		const clockIn = getPunchTimestampMinutes(punchesObj, PunchType.CLOCK_IN)
-		const clockOut = getPunchTimestampMinutes(punchesObj, PunchType.CLOCK_OUT)
-		const journeyTime = clockOut - clockIn
-		
-		const lunchIn = getPunchTimestampMinutes(punchesObj, PunchType.START_LUNCH)
-		const lunchOut = getPunchTimestampMinutes(punchesObj, PunchType.END_LUNCH)
-		const lunchTime = lunchOut - lunchIn
+      accumulator[date].punches.push(punch);
+      return accumulator;
+    },
+    {} as Record<string, GroupedPunchesType>,
+  );
 
-		const workedTime = journeyTime - lunchTime
+  const punchesArray = Object.values(groupedPunches).reverse();
 
-		return {
-			...punchesObj,
-			workedTime,
-		}
-	})
-	
-	return result
+  const result = punchesArray.map((punchesObj) => {
+    const clockIn = getPunchTimestampMinutes(punchesObj, PunchType.CLOCK_IN);
+    const clockOut = getPunchTimestampMinutes(punchesObj, PunchType.CLOCK_OUT);
+    const journeyTime = clockOut - clockIn;
+
+    const lunchIn = getPunchTimestampMinutes(punchesObj, PunchType.START_LUNCH);
+    const lunchOut = getPunchTimestampMinutes(punchesObj, PunchType.END_LUNCH);
+    const lunchTime = lunchOut - lunchIn;
+
+    const workedTime = journeyTime - lunchTime;
+
+    return {
+      ...punchesObj,
+      workedTime,
+    };
+  });
+
+  return result;
 }
 
 export function overtimeUndertime(workTime: number, workedTime: number) {
-	const overtime = workedTime - workTime
-	const timeStr = minutesToTimeString(Math.abs(overtime))
+  const overtime = workedTime - workTime;
+  const timeStr = minutesToTimeString(Math.abs(overtime));
 
-	const underOver = isUnderOver(overtime)
-	return {
-		...underOver,
-		timeStr,
-	}	
+  const underOver = isUnderOver(overtime);
+  return {
+    ...underOver,
+    timeStr,
+  };
 }
 
 /*export function getTotalOvertime(punches: PunchesPerDayType[], schedules: {dayOfWeek: number; workTime: number;}[], initialBalance: number) {
@@ -82,61 +95,79 @@ export function overtimeUndertime(workTime: number, workedTime: number) {
 	}
 }*/
 
-export function getTotalOvertime(punches: PunchesPerDayType[], schedules: {dayOfWeek: number; workTime: number;}[], justifications: JustificationByDayType[], initialBalance: number) {
-	const justificationsSum = justifications.reduce((accumulator, justification) => {
-		return accumulator + justification.timeMinutes
-	}, 0)
+export function getTotalOvertime(
+  punches: PunchesPerDayType[],
+  schedules: { dayOfWeek: number; workTime: number }[],
+  justifications: JustificationByDayType[],
+  initialBalance: number,
+) {
+  const justificationsSum = justifications.reduce(
+    (accumulator, justification) => {
+      return accumulator + justification.timeMinutes;
+    },
+    0,
+  );
 
-	const punchesSum = punches.reduce((accumulator, day) => {
-		const daySchedule = schedules.find((schedule) => schedule.dayOfWeek === day.dayOfWeek)
+  const punchesSum = punches.reduce((accumulator, day) => {
+    const daySchedule = schedules.find(
+      (schedule) => schedule.dayOfWeek === day.dayOfWeek,
+    );
 
-		const workTime = daySchedule ? daySchedule.workTime : 0
-		const overUnder = overtimeUndertime(workTime, day.workedTime)
-		
-		return accumulator + overUnder.time
-	}, 0)
+    const workTime = daySchedule ? daySchedule.workTime : 0;
+    const overUnder = overtimeUndertime(workTime, day.workedTime);
 
-	const total = punchesSum + justificationsSum + initialBalance
-	const timeStr = minutesToTimeString(Math.abs(total))
+    return accumulator + overUnder.time;
+  }, 0);
 
-	const underOver = isUnderOver(total)
+  const total = punchesSum + justificationsSum + initialBalance;
+  const timeStr = minutesToTimeString(Math.abs(total));
 
-	return {
-		...underOver,
-		timeStr,
-	}
+  const underOver = isUnderOver(total);
+
+  return {
+    ...underOver,
+    timeStr,
+  };
 }
 
-export async function getWorkdayBalanceReport(userId: string, initialDate: Date, finalDate: Date, dailySchedulesTime: dailySchedulesTimeType[]) {
-	const punchesPerDay = await groupPunchesByDay(userId)
-	
-	const punchesPerDayMap = new Map<string, PunchesPerDayType>(
-		punchesPerDay.map(day => [day.date, day])
-	)
-	const dailySchedulesTimeMap = new Map<number, dailySchedulesTimeType>(
-		dailySchedulesTime.map(day => [day.dayOfWeek, day])
-	)
+export async function getWorkdayBalanceReport(
+  userId: string,
+  initialDate: Date,
+  finalDate: Date,
+  dailySchedulesTime: dailySchedulesTimeType[],
+) {
+  const punchesPerDay = await groupPunchesByDay(userId);
 
-	const currentDate = new Date(initialDate)
-	const finalReportList = []
-	while (currentDate <= finalDate) {
-		const currentDateString = formatDate(currentDate)
-		const currentDay = currentDate.getDay()
-		const currentPunch = punchesPerDayMap.get(currentDateString)
-		
-		if (currentPunch) {
-			finalReportList.push(currentPunch)
-		} else if (punchesPerDay.length > 0 && dailySchedulesTimeMap.has(currentDay)){
-			finalReportList.push({
-				workedTime: 0,
-				timestamp: new Date(currentDate),
-				date: currentDateString,
-				dayOfWeek: currentDay,
-				punches: []
-			})
-		}
-		currentDate.setDate(currentDate.getDate() + 1)
-	}
-	
-	return finalReportList
+  const punchesPerDayMap = new Map<string, PunchesPerDayType>(
+    punchesPerDay.map((day) => [day.date, day]),
+  );
+  const dailySchedulesTimeMap = new Map<number, dailySchedulesTimeType>(
+    dailySchedulesTime.map((day) => [day.dayOfWeek, day]),
+  );
+
+  const currentDate = new Date(initialDate);
+  const finalReportList = [];
+  while (currentDate <= finalDate) {
+    const currentDateString = formatDate(currentDate);
+    const currentDay = currentDate.getDay();
+    const currentPunch = punchesPerDayMap.get(currentDateString);
+
+    if (currentPunch) {
+      finalReportList.push(currentPunch);
+    } else if (
+      punchesPerDay.length > 0 &&
+      dailySchedulesTimeMap.has(currentDay)
+    ) {
+      finalReportList.push({
+        workedTime: 0,
+        timestamp: new Date(currentDate),
+        date: currentDateString,
+        dayOfWeek: currentDay,
+        punches: [],
+      });
+    }
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return finalReportList;
 }
